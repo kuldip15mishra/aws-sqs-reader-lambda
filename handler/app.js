15821c592c9fb0ff29310abe2c9a03d8d4461a11
Kuldip Mishra <kuldipmishra@Kuldips-Air.ht.home>
2023-06-29 23:46:46 -0400

    sampel

---
+++ handler/app.js
@@ -0,0 +1,18 @@
+exports.handler = async function (event, context, callback) {
+    let arrItems = [4, 5, 6, 8, 9, 10, 35, 70, 80, 31];
+    function countevennumbers(items) {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                let a = 0;
+                for (var i in items) {
+                    if (items[i] % 2 == 0) {
+                        a++;
+                    }
+                }
+                resolve(a);
+            }, 2000);
+        });
+    }
+    let evennumber = await countevennumbers(arrItems);
+    callback(null, 'even numbers equals =' + evennumber);
+};
